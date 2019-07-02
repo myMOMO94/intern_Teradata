9f360d67011b6554ecce701e8fe28a495197ed4d
Mengyu Mo <mm250182@musmm250182-539.td.teradata.com>
2019-07-02 10:27:38 -0700

    add interactive flag to choose the  mode you want

--- tcp_client_server/tcp_client.go
+++ tcp_client_server/tcp_client.go
@@ -5,49 +5,64 @@ import "os"
 import "bufio"
 import "fmt"
 import "log"
+import "flag"
+import "strconv"
 
 func main() {
   //initialize connection
-  /*address_reader := bufio.NewReader(os.Stdin)
-  fmt.Print("Ip and port to connect: (follow the pattern 127.0.0.1:8080): ")
-  address, err_add := address_reader.ReadString('\n')
-  if (err_add != nil) {
-    log.Fatal(err_add)
-  }
-  fmt.Print("Connecting to Ip: " + string(address))*/
+  //choose the mode, either interactive or not interactive
+  interactivePtr := flag.Bool("interactive", false, "a bool")
+  flag.Parse()
 
-  scanner := bufio.NewScanner(os.Stdin)
-  var address string
-  fmt.Print("Ip and port to connect(follow the pattern 127.0.0.1:8080): ")
-  scanner.Scan()
-  address = scanner.Text()
+  //find the ip address and num of bytes
+  address := os.Args[2]
+  numbytes,_ := strconv.Atoi(os.Args[3])
+
+  //connect to server
   conn, err := net.Dial("tcp", address) //"127.0.0.1:8080")
   fmt.Println("Connected to Ip: " + address)
   if (err != nil) {
     log.Fatal(err)
   }
 
-  for {
-    //read input message from stdin
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
-    text, err_text := reader.ReadString('\n')
-    if (err_text != nil) {
-      log.Fatal(err_text)
-    }
+  //interactive flag is true
+  if *interactivePtr == true {
+    for {
+      //read input message from stdin
+      reader := bufio.NewReader(os.Stdin)
+      fmt.Print("Text to send: ")
+      text, err_text := reader.ReadString('\n')
+      if (err_text != nil) {
+        log.Fatal(err_text)
+      }
 
-    //send message to server
-    numbytes, err_sent := fmt.Fprintf(conn, text + "\n")
+      //send message to server
+      nbytes, err_sent := fmt.Fprintf(conn, text + "\n")
+      if (err_sent != nil) {
+        log.Fatal(err_sent)
+      }
+      fmt.Print("Sent ", nbytes, " bytes Message to server: " + text)
+
+      //Listen for sever reply
+      message, err_read := bufio.NewReader(conn).ReadString('\n')
+      if (err_read != nil) {
+        log.Fatal(err_read)
+      }
+      fmt.Print("Message from server: " + message)
+    }
+  } else {
+    //interactive flag is false
+    data := make([]byte, numbytes)
+    nbytes, err_sent := conn.Write(data)
     if (err_sent != nil) {
       log.Fatal(err_sent)
     }
-    fmt.Print("Sent ", numbytes, " bytes Message to server: " + text)
+    fmt.Println("Sent ", nbytes, " bytes Message to server")
 
-    //Listen for sever reply
-    message, err_read := bufio.NewReader(conn).ReadString('\n')
+    //Listen for server reply
+    /*_, err_read := bufio.NewReader(conn).ReadString('\n')
     if (err_read != nil) {
       log.Fatal(err_read)
-    }
-    fmt.Print("Message from server: " + message)
+    }*/
   }
 }
--- tcp_client_server/tcp_server.go
+++ tcp_client_server/tcp_server.go
@@ -4,17 +4,23 @@ import "net"
 import "fmt"
 import "log"
 import "bufio"
+import "io/ioutil"
+import "flag"
 
 func main() {
   fmt.Println("Launching Server...")
-  
+
+  //parse interactive flag
+  interactivePtr := flag.Bool("interactive", false, "a bool")
+  flag.Parse()
+
   //listen on all interfaces
   ln, err := net.Listen("tcp", ":8080")
   if (err != nil) {
     // handle error
     log.Fatal(err)
   }
-  
+
   //accept connection on port
   conn, err_conn := ln.Accept()
   if (err_conn != nil) {
@@ -22,18 +28,51 @@ func main() {
     log.Fatal(err_conn)
   }
 
-  for {
-    // will listen for message to process ending in newline delim
-    message, err_rec := bufio.NewReader(conn).ReadString('\n')
-    if (err_rec != nil) {
-      log.Fatal(err_rec)
+  if *interactivePtr == true {
+    for {
+      // will listen for message to process ending in newline delim
+      message, err_rec := bufio.NewReader(conn).ReadString('\n')
+      if (err_rec != nil) {
+        log.Fatal(err_rec)
+      }
+      fmt.Print("Message received: ", string(message))
+
+      //send message back to client
+      _, err_send := conn.Write([]byte(string(message) + "\n"))
+      if (err_send != nil) {
+        log.Fatal(err_send)
+      }
+      /*const MaxUint = ^uint(0)
+      const MaxInt = int(MaxUint >> 1)
+      data := make([]byte, MaxInt)
+      numBytes, err_read := conn.Read(data)
+      if (err_read != nil) {
+        log.Fatal(err_read)
+      }
+      data = data[:0]
+      fmt.Println(numBytes, "bytes data received from client")
+
+      //send response message back to client
+      message := make([]byte, numBytes)
+      _, err_sent := conn.Write(message)
+      if (err_sent != nil) {
+        log.Fatal(err_sent)
+      }
+      message = nil*/
     }
-    fmt.Print("Message received: ", string(message))
-    
-    //send message back to client
-    _, err_send := conn.Write([]byte(string(message) + "\n"))
-    if (err_send != nil) {
-      log.Fatal(err_send)
+  } else {
+    reader := bufio.NewReader(conn)
+    b, err := ioutil.ReadAll(reader)
+    if (err != nil) {
+      log.Fatal(err)
+    }
+    fmt.Println(len(b), " bytes data received from client.")
+
+    //sent response back to client
+    data := make([]byte, len(b))
+    _, err_sent := conn.Write(data)
+    if (err_sent != nil) {
+      log.Fatal(err_sent)
     }
   }
 }
