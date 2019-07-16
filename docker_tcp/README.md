ef8124a1a5db2279cd9fcaad517368a8d6014a2a
Mengyu Mo <mm250182@musmm250182-539.td.teradata.com>
2019-07-16 14:40:18 -0700

    Added tcp-client-server with docker network

--- docker_tcp/README.md
+++ docker_tcp/README.md
@@ -2,8 +2,8 @@
 
 *You can pull the images here:*
 ```
-> momomengyu/http-client:part2
-> momomengyu/http-server:part2
+> momomengyu/tcp-client:part2
+> momomengyu/tcp-server:part2
 ```
 *Or, you can build the images by yourself*
 ```
@@ -14,15 +14,15 @@
 *To run, please type the following command on your terminal:*\
 *At first, please create a Docker user defined bridge network:*
 ```
-> docker network create --driver bridge http-net
+> docker network create --driver bridge tcp-net
 ```
 **(You can change the name of the network if you want)**
 
 *Then, please run the following command (this will attach containers to the network you created above):*
 ```
-> docker run -dit --name http-server --network http-net momomengyu/http-server:part2 -verbose=false
+> docker run -dit --name tcp-server --network tcp-net -e NUMRUNS="2" momomengyu/tcp-server:part2 -verbose=false
 ```
-**(Please do not change the server container name)**
+**(Please do not change the server container name, however, you can change the NUMRUNS if you want, just keep it consistent with NUMRUNS below.)**
 ```
-> docker run -it --name http-client --network http-net -e NUMBYTES="512" -e NUMRUNS="2" momomengyu/http-client:part2 -verbose=false
+> docker run -it --name tcp-client --network tcp-net -e NUMBYTES="512" -e NUMRUNS="2" momomengyu/tcp-client:part2 -verbose=false
 ```
---
+++ docker_tcp/server/tcp_server.go
@@ -0,0 +1,140 @@
+package main
+
+import "net"
+import "fmt"
+import "log"
+import "bufio"
+import "os"
+import "flag"
+import "strconv"
+
+// convert string bytes info to integer
+func Atoi (s string) int {
+  var (
+    n uint64
+    i int
+    v byte
+  )
+
+  for ; i < len(s); i++ {
+    d := s[i]
+    if '0' <= d && d <= '9' {
+      v = d - '0'
+    } else {
+      break
+    }
+    n *= uint64(10)
+    n += uint64(v)
+
+  }
+  //fmt.Println("int number: ", int(n))
+  return int(n)
+}
+
+func main() {
+  fmt.Println("Launching Server...")
+
+  /*if (len(os.Args) != 2) {
+    fmt.Println("Please follow the pattern ./tcp_server -interactive=true, not the exact number of arguments as expected!")
+    return
+  }*/
+  numruns, err_run := strconv.Atoi(os.Args[3])
+  if (err_run != nil) {
+    log.Fatal(err_run)
+  }
+
+  //parse interactive flag
+  interactivePtr := flag.Bool("interactive", false, "a bool")
+  verbosePtr := flag.Bool("verbose", false, "a bool")
+  flag.Parse()
+
+  //listen on all interfaces
+  ln, err := net.Listen("tcp", ":8080")
+  if (err != nil) {
+    // handle error
+    log.Fatal(err)
+  }
+
+  //accept connection on port
+  conn, err_conn := ln.Accept()
+  if (err_conn != nil) {
+    // handle error
+    log.Fatal(err_conn)
+  }
+
+  if *interactivePtr == true {
+    if *verbosePtr == true {
+      fmt.Println("SERVER: You are in the interactive mode, which allows you to type in a payload.")
+    }
+    for {
+      // will listen for message to process ending in newline delim
+      message, err_rec := bufio.NewReader(conn).ReadString('\n')
+      if (err_rec != nil) {
+        log.Fatal(err_rec)
+      }
+      if *verbosePtr == true {
+        fmt.Print("SERVER: Message received: ", string(message))
+      }
+
+      //send message back to client
+      _, err_send := conn.Write([]byte(string(message) + "\n"))
+      if (err_send != nil) {
+        log.Fatal(err_send)
+      }
+    }
+  } else {
+    if *verbosePtr == true {
+      fmt.Println("SERVER: You are in the non-interactive mode!")
+    }
+    for i := 0; i < numruns; i++ {
+      //will listen for message from client
+      //get number of bytes info first
+      nbytes, err_bytes := bufio.NewReader(conn).ReadString('\n')
+      if (err_bytes != nil) {
+        log.Fatal(err_bytes)
+      }
+      numbytes := Atoi(string(nbytes))
+      if *verbosePtr == true {
+        fmt.Println("SERVER: Received bytes info:  ", numbytes, " bytes. ")
+      }
+
+      // tell client that server received bytes info
+      _, err_byt := conn.Write([]byte(string("Received bytes info.") + "\n"))
+      if (err_byt != nil) {
+        log.Fatal(err_byt)
+      }
+      if *verbosePtr == true {
+        fmt.Println("SERVER: Sent received bytes info message back.")
+      }
+
+      //will listen for message from client
+      data := make([]byte, numbytes)//32*1024)
+      numBytes, err_read := conn.Read(data)
+      if (err_read != nil) {
+        log.Fatal(err_read)
+      }
+      for ; numBytes < numbytes; {
+        n, err := conn.Read(data)
+        if (err != nil) {
+          log.Fatal(err)
+        }
+        numBytes += n
+      }
+      data = nil
+      if *verbosePtr == true {
+        fmt.Println("SERVER: ", numBytes, " bytes data received from client")
+      }
+
+      //send response message back to client
+      message := make([]byte, numBytes)
+      numbyte, err_sent := conn.Write(message)
+      if (err_sent != nil) {
+        log.Fatal(err_sent)
+      }
+      message = nil
+      if *verbosePtr == true {
+        fmt.Println("SERVER: sent ", numbyte, " bytes back to client.")
+      }
+    }
+  }
+}
